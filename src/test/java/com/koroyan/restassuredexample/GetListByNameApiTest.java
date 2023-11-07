c09b62bc00b88aa99593b3092b508122115d360c
git config <sandro.gagnidze.1@btu.edu.ge>
2023-11-07 17:33:35 +0400

    Update project again

--- src/test/java/com/koroyan/restassuredexample/GetListByNameApiTest.java
+++ src/test/java/com/koroyan/restassuredexample/GetListByNameApiTest.java
@@ -10,17 +10,15 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.testng.annotations.Test;
 
 public class GetListByNameApiTest {
-    private Step step;
-    private ListByNameRepository listByNameRepository;
+    private Step step = new Step();
+    private ListByNameRepository listByNameRepository = new ListByNameImpl();
 
     @Test
     void mainTest() throws JSONException {
-        listByNameRepository = new ListByNameImpl();
-        step = new Step();
-
         GetListByNameResult actualList = step.getListByName(CommonData.NAME);
         GetListByNameResult expectedList = listByNameRepository.getListByName(CommonData.NAME);
         JSONAssert.assertEquals(actualList.toString(), expectedList.toString(), false);
+
     }
 
 }
